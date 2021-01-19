008e88e841901067e46d3e129cc5c187c9e6636c
Shaishav P <shaishav2019@gmail.com>
2021-01-18 19:20:05 -0700

    Created basic UI

---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <div>
+            <h5>Your Balance</h5>
+            <h2 id="balance">$0.00</h2>
+        </div>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//function cmpt
+export const Header = () => {
+    return (
+        <h3>
+            My Expense Tracker
+        </h3>
+    )
+}
